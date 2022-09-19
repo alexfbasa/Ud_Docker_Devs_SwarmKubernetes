a8fc87ffa36952d089de42d77725d96b29aa8602
alexandre.basilio <alexandre.basilio@kuehne-nagel.com>
2022-09-19 15:15:26 +0300

    Changing Flask

--- 01_externa/app.py
+++ 01_externa/app.py
@@ -1,4 +1,5 @@
 import flask
+from flask import request, json, jsonify
 import requests
 
 app = flask.Flask(__name__)
@@ -10,5 +11,6 @@ def index():
     data = requests.get('https://randomuser.me/api')
     return data.json()
 
-if __name__ == "__main__"
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port="5000")
