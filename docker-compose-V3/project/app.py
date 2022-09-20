daccb2b5287369515cc8f21fb976d1d6c85881f8
AlexBasa <alexsimple@gmail.com>
2022-09-20 22:05:29 +0300

    Added new env files

--- docker-compose-V3/project/app.py
+++ docker-compose-V3/project/app.py
@@ -1,16 +1,12 @@
 import flask
-from flask import request, json, jsonify
-import requests
+from flask import Flask, render_template
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
-@app.route("/", methods=["GET"])
+@app.route('/')
 def index():
-    data = requests.get('https://randomuser.me/api')
-    return data.json()
-
+  return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port="5000")
+  app.run(host="0.0.0.0", debug=True, port="5000")
